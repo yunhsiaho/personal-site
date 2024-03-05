4517c072bf956663c480097b338f117e111ae13d
yunhsiaho <yunhsia.ho@gmail.com>
2024-03-05 22:08:38 +0100

    finish footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,17 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="content">
-        <div className="content-img">
-          <img src={yhlogo} alt="logo" />
-        </div>
-        <div className="content-name">
+        <div className="content-container">
+          {/* <div className="content-img"> */}
+            <img src={yhlogo} alt="logo" />
+          {/* </div> */}
           <div className="content-name-p bungee-regular">
             <p>yun-hsia</p>
             <p>ho</p>
           </div>
         </div>
       </div>
-      <p className='bungee-hairline-regular '>Made with love by YUN-HSIA HO</p>
+      <p className='bungee-hairline-regular footer-line'>Made with love by YUN-HSIA HO</p>
     </div>
   )
 }
