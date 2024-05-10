bb353a30c6eeaf30e1020f0229271828dfac172c
yunhsiaho <yunhsia.ho@gmail.com>
2024-05-10 13:15:48 +0200

    adding about photos

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import p1 from '../assets/p12.jpg';
+import p2 from '../assets/p13.jpeg';
 
 const About = () => {
   return (
@@ -14,8 +15,8 @@ const About = () => {
       My enthusiasm for web development and UI/UX design equips me with the skills to communicate with the world.
       </p>      
       <div className="about-img">
-        <img className="photo1" src="" alt="" />
-        <img className="photo2" src="" alt="" />
+        <img className="about-photo-left" src={p1} alt="#" />
+        <img className="about-photo-right" src={p2} alt="#" />
       </div>
       </div>
     </div>
