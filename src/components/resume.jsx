3353102badd81622d7c98867d32b59b0ae6d3ac9
yunhsiaho <yunhsia.ho@gmail.com>
2024-08-25 18:25:40 +0200

    new push

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -4,6 +4,17 @@ const Resume = () => {
   return (
     <div className="resume-container container">
             <h1 className='bungee-regular'>resume</h1>
+            <div className="resume">
+              <div className="skill">
+
+              </div>
+              <div className="resume-file">
+
+              </div>
+              <div className="language">
+                
+              </div>
+            </div>
     </div>
   )
 }
